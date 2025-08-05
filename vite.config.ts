b0992043a14f86e85305687d54d584e7d0c0df89
Lauris Locans <laurislocans@gmail.com>
2025-08-05 15:39:21 +0300

    Launch public website

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/wireback-website/',
   plugins: [
     vue(),
     //vueDevTools(),
